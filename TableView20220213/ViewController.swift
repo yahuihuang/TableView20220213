6eea9b5f0cd9f0c049f0abeee4bd367e39a8070d
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-13 11:00:59 +0800

    cell內加子UIView

--- TableView20220213/ViewController.swift
+++ TableView20220213/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         myTableView.dataSource = self
         myTableView.delegate = self
+        
+        // 分隔線的樣式
+        myTableView.separatorStyle = .singleLine
+        
+        // 分隔線的間距 四個數值分別代表 上、左、下、右 的間距
+        myTableView.separatorInset = UIEdgeInsets.init(top: 0, left: 20, bottom: 0, right: 20)
+        
+        // 是否可以點選 cell
+        myTableView.allowsSelection = true
+        
+        // 是否可以多選 cell
+        myTableView.allowsMultipleSelection = false
     }
     
     //MARK: UITableViewDataSource, UITableViewDelegate
@@ -34,7 +46,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "theTableViewCell")!
         cell.backgroundColor = UIColor.red
         cell.textLabel?.text = "\(indexPath)"
+        
+        let theView = UIView(frame: CGRect(x: 10, y: 10, width: 250, height: 30))
+        theView.backgroundColor = UIColor.yellow
+        
+        cell.addSubview(theView)
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
     }
     
 }
