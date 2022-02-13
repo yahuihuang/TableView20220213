d548d4d939c14eb97b2a2116b0fceafa615a7258
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-13 10:19:21 +0800

    random of cell

--- TableView20220213/ViewController.swift
+++ TableView20220213/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+//        return 5
+        var numberOfRows = Int.random(in: 1...10)
+        return numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
