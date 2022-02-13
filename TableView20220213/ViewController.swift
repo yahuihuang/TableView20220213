6d685669721ded68de0d0b8f5dafefc465ea3d3d
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-13 10:40:02 +0800

    Add a UITableViewCell

--- TableView20220213/ViewController.swift
+++ TableView20220213/ViewController.swift
@@ -16,17 +16,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myTableView.dataSource = self
         myTableView.delegate = self
     }
+    
+    //MARK: UITableViewDataSource, UITableViewDelegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 5
-        var numberOfRows = Int.random(in: 1...10)
-        return numberOfRows
+        return 5
+//        var numberOfRows = Int.random(in: 1...10)
+//        return numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let redCell = UITableViewCell()
-        redCell.backgroundColor = UIColor.yellow
-        return redCell
+//        let redCell = UITableViewCell()
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "theTableViewCell")!
+        cell.backgroundColor = UIColor.red
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
     }
     
 }
